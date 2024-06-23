007bfa6b1237a1e09cf3173369ebd2b16b8be291
dhanushcodez <112488177+dhanushcodez@users.noreply.github.com>
2024-06-23 12:25:21 +0530

    Update AdminOrders.js

--- src/pages/Admin/AdminOrders.js
+++ src/pages/Admin/AdminOrders.js
@@ -21,7 +21,7 @@ const AdminOrders = () => {
   const [auth, setAuth] = useAuth();
   const getOrders = async () => {
     try {
-      const { data } = await axios.get("/api/v1/auth/all-orders");
+      const { data } = await axios.get("https://e-commerce-server-ydqb.onrender.com/api/v1/auth/all-orders");
       setOrders(data);
     } catch (error) {
       console.log(error);
